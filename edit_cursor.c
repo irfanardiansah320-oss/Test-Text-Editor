1f80ae2bd80645aa5ab059cb8600fd71bb1ba204
irfanardiansah320-oss <irfanardiansah320@gmail.com>
2026-04-08 20:39:19 +0700

    fix

--- edit_cursor.c
+++ edit_cursor.c
@@ -7,8 +7,8 @@
 #include "zidan.h"
 #include "irfan1.h"
 
-#define MAX_ROWS 100
-#define MAX_COLS 256
+#define MAX_ROWS 10
+#define MAX_COLS 5
 
 static char text[MAX_ROWS][MAX_COLS];
 static int rowCount = 1;
