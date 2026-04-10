91e0fb181d41e8043a2b20268d87741c4586f88b
sahizidanlukman <sahizidanlukman@gmail.com>
2026-04-10 16:33:23 +0700

    revisi zidan.c

--- zidan.c
+++ zidan.c
@@ -63,6 +63,10 @@ void findAndReplace() {
     fclose(fp);
 
     printf("Berhasil replace kata!\n");
+    // CLEAR BUFFER
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+
     runEditor(filename, 0);
 }
 
