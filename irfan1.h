684698b59e25c7479dfe7e4fce3cb1880fce62fc
sahizidanlukman <sahizidanlukman@gmail.com>
2026-05-04 21:09:52 +0700

    150

--- irfan1.h
+++ irfan1.h
@@ -3,6 +3,6 @@
 
 void createNewFile();
 void exitEditor ();
-void handleTextEditing(int ch, char text[][256], int *cursorRow, int *cursorCol, int *rowCount);
+void handleTextEditing(int ch, char text[][150], int *cursorRow, int *cursorCol, int *rowCount);
 
 #endif
